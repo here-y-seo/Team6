246480f287715843d774351c4e205258ac5f53f9
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-08-27 17:36:56 +0900

    initial commit

---
+++ SMSH-Summer/SMSH-Summer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SMSH-Summer
+//
+//  Created by 강윤서 on 2022/08/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
