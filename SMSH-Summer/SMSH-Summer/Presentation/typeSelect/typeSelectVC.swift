c4eff844d7a384c7dbb892976661cd31e93b4489
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-08-28 20:31:55 +0900

    [Fix] #31 - 모서리 수정

--- SMSH-Summer/SMSH-Summer/Presentation/typeSelect/typeSelectVC.swift
+++ SMSH-Summer/SMSH-Summer/Presentation/typeSelect/typeSelectVC.swift
@@ -16,7 +16,7 @@ class typeSelectVC: UIViewController {
     private let typeSelectView = UIView().then {
         $0.backgroundColor = .white
         $0.layer.cornerRadius = 20
-        $0.layer.maskedCorners = [.layerMinXMinYCorner, .layerMinXMaxYCorner]
+        $0.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
     }
     
     private let selectTitleLabel = UILabel().then {
@@ -80,7 +80,7 @@ class typeSelectVC: UIViewController {
 
 extension typeSelectVC {
     private func setUI() {
-        self.view.backgroundColor = .clear
+        self.view.backgroundColor = .clear.withAlphaComponent(60)
     }
     
     private func setStackView() {
