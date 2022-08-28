432939b0f6b5dcf64ac379dbfdfbea1be40b8f50
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-08-28 10:30:01 +0900

    [Add] #8 - 이미지 리터럴 추가

--- SMSH-Summer/SMSH-Summer/Global/Literals/ImageLiterals.swift
+++ SMSH-Summer/SMSH-Summer/Global/Literals/ImageLiterals.swift
@@ -12,29 +12,20 @@ struct ImageLiterals {
         static let splashIcon = UIImage(named: "splashIcon")
     }
     
-    struct Common {
-        static let starIcon = UIImage(named: "btn_star_01")
-        static let starIcon_25 = UIImage(named: "btn_star_02")
-        static let starIcon_50 = UIImage(named: "btn_star_03")
-        static let starIcon_75 = UIImage(named: "btn_star_04")
-        static let starIcon_filled = UIImage(named: "btn_star_05")
-    }
-    
-    struct MainDetail {
-        static let scrapIcon = UIImage(named: "icn_scrap")
-        static let scrapIcon_filled = UIImage(named: "icn_scrap_filled")
-        static let beforeIcon = UIImage(named: "icn_before")
-        static let starIcon = UIImage(named: "icn_star")
-        static let starIcon_filled = UIImage(named: "icn_star_filled")
-        static let directionIcon = UIImage(named: "icn_direction")
-        static let locationIcon = UIImage(named: "icn_location")
-        static let phoneIcon = UIImage(named: "icn_phone")
-        static let showdownIcon = UIImage(named: "icn_showdown")
-        static let showupIcon = UIImage(named: "icn_showup")
-        static let timeIcon = UIImage(named: "icn_time")
-
-        static let tempSalady = UIImage(named: "tempSalady")
-        static let tempMuseum = UIImage(named: "tempMuseum")
+    struct Write {
+        static let animalBtn = UIImage(named: "animalBtn")
+        static let animalDisable = UIImage(named: "animalDisableBtn")
+        static let donationBtn = UIImage(named: "donationBtn")
+        static let donationDisableBtn = UIImage(named: "donationDisableBtn")
+        static let etcBtn = UIImage(named: "etcBtn")
+        static let etcDisableBtn = UIImage(named: "etcDisableBtn")
+        static let ploggingBtn = UIImage(named: "ploggingBtn")
+        static let ploggingDisableBtn = UIImage(named: "ploggingDisableBtn")
+        static let volunteerBtn = UIImage(named: "volunteerBtn")
+        static let volunteerDisableBtn = UIImage(named: "volunteerDisableBtn")
+        static let plusBtn = UIImage(named: "plusBtn")
+        static let deleteBtn = UIImage(named: "deleteBtn")
+        static let calendar = UIImage(named: "calendar")
     }
     
 }
