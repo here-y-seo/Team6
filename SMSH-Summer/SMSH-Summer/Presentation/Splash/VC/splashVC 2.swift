a107c97496194246c89972c47ecbbc639f4042ce
Doy Kim <kimdee.dev@gmail.com>
2022-08-28 20:24:19 +0900

    선행 구조체 선언, 리스트 추가

---
+++ SMSH-Summer/SMSH-Summer/Presentation/Splash/VC/splashVC 2.swift
@@ -0,0 +1,61 @@
+//
+//  SplashVC.swift
+//  SMSH-Summer
+//
+//  Created by 강윤서 on 2022/08/28.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class splashVC: UIViewController {
+    
+    // MARK: - UI Components
+    
+    private let splashView = UIView().then {
+        $0.backgroundColor = .secondaryColor
+    }
+    
+    private let titleLabel = UILabel().then {
+        $0.textColor = .white
+        $0.text = I18N.Splash.title
+        $0.font = .GhanaRegular(ofSize: 44)
+        $0.numberOfLines = 2
+    }
+    
+    private let logoImage = UIImageView().then {
+        $0.image = ImageLiterals.Splash.splashIcon
+    }
+    
+    // MARK: - View Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setLayout()
+    }
+
+}
+
+// MARK: Extension
+
+extension splashVC {
+    private func setLayout() {
+        view.addSubviews(splashView)
+        splashView.addSubviews(titleLabel, logoImage)
+        
+        splashView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).inset(70)
+            make.leading.equalTo(view.safeAreaLayoutGuide).inset(67)
+        }
+        
+        logoImage.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(11)
+            make.leading.equalToSuperview().inset(52)
+        }
+    }
+}
