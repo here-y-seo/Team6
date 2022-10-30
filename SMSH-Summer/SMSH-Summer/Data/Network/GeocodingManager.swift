8e20ecb9062b25a6cf0a1b521cbf9000457b6759
Doy Kim <kimdee.dev@gmail.com>
2022-10-30 23:02:54 +0900

    [Add] #39 지오코딩 통신 담당하는 클래스파일 추가

---
+++ SMSH-Summer/SMSH-Summer/Data/Network/GeocodingManager.swift
@@ -0,0 +1,15 @@
+//
+//  GeocodingManager.swift
+//  SMSH-Summer
+//
+//  Created by Doy Kim on 2022/10/30.
+//
+
+import Foundation
+
+class GeocodingManager {
+    static let shared = GeocodingManager()
+    
+    
+}
+
